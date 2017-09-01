ee529393540e7060cb0b5d55ddd1b137d3703c8a
dasigelchen <chris.defalco@gmail.com>
2017-09-01 09:42:21 -0400

    Modified project1

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-  cout << "Hello World!";
+  string name;
+  string hello = "Hello, ";
+  string meet = ", nice to meet you!\n";
+  string greeting;
+
+  cout << "What is your name? ";
+  cin >> name;
+
+  greeting = hello + name + meet;
+
+  cout << greeting;
+
   return 0;
 }
