39106df2cd04a47f0e45ae07b54efd35366ff649
dasigelchen <chris.defalco@gmail.com>
2017-09-01 10:06:32 -0400

    Modified project3

---
+++ project3/project3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+  string quote, speaker;
+
+  cout << "What is the quote? ";
+  getline(cin, quote);
+
+  cout << "Who said it? ";
+  getline(cin, speaker);
+
+  cout << speaker << " says, \"" << quote << "\"\n";
+  return 0;
+}
